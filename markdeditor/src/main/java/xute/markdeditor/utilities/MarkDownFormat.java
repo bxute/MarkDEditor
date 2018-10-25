fc0eefb48c1a5d4d6393baedd3cef308a8fea245
Ankit Kumar <ankit.kumar071460@gmail.com>
2018-10-25 14:06:02 +0530

    fix hint of text input

--- markdeditor/src/main/java/xute/markdeditor/utilities/MarkDownFormat.java
+++ markdeditor/src/main/java/xute/markdeditor/utilities/MarkDownFormat.java
@@ -37,11 +37,11 @@ public class MarkDownFormat {
   }
 
   public static String getImageFormat(String url) {
-    return String.format("<br>\\n<center>![Image](%s)</center>", url);
+    return String.format("\\n<center>![Image](%s)</center>", url);
   }
 
   public static String getCaptionFormat(String caption) {
-    return caption != null ? String.format("<center>%s</center>\\n\\n<br>", caption) : "\\n\\n<br><br>";
+    return caption != null ? String.format("<center>%s</center>\\n\\n\\n", caption) : "\\n\\n\\n";
   }
 
   public static String getLineFormat() {
